7a8cce0cfd63b146d431761347d6b66407b72066
Daniel Boniface <danielboniface344@gmail.com>
2026-03-25 00:03:43 +0300

    Test to code scanning

---
+++ passord.java
@@ -0,0 +1,7 @@
+public class HardcodedPassword {
+    public static void main(String[] args) {
+        //Hardcoded credentials 
+String passsword = "123456";
+System.out.println("My password is " +password);
+}
+}
